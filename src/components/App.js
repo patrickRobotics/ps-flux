27c1b63d02c85a3469bce9bf0b88a3c7f660e52b
patrickRobotics <pirungu9@gmail.com>
2020-02-07 17:36:41 +0300

    simple routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 
+import Header from "./common/Header.js";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import { func } from "prop-types";
 
 function App() {
-  const route = window.location.pathname;
-  if (route === "/about") return <AboutPage />;
-  return <HomePage />;
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,6 @@ function HomePage() {
     <div className="jumbotron">
       <h1>Courses Administration</h1>
       <p>React, FLux, and React Router for ultra-responsive web apps</p>
-      <a href="/about">About</a>
     </div>
   );
 }
