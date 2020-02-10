111d4611032fae3b6b08512dae283da2cceb35e4
patrickRobotics <pirungu9@gmail.com>
2020-02-10 18:23:15 +0300

    add courses page and getCourses

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import React from "react";
 import Header from "./common/Header.js";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
-import { func } from "prop-types";
+import CoursesPage from "./CoursesPage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
     if (route === "/about") return <AboutPage />;
+    if (route === "/courses") return <CoursesPage />;
     return <HomePage />;
   }
 
---
+++ src/components/CoursesPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { getCourses } from "../api/courseApi";
+
+class CoursesPage extends React.Component {
+  state = {
+    courses: []
+  };
+
+  componentDidMount() {
+    getCourses().then(courses => this.setState({ courses: courses }));
+  }
+
+  render() {
+    return <h2>Courses</h2>;
+  }
+}
+
+export default CoursesPage;
